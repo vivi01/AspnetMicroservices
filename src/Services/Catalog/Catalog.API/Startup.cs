8809e0699bf5d509b4a314daf8be7dcebfea5056
Viviane dos Santos <vivianecruz01@hotmail.com>
2021-04-26 20:42:54 -0300

    implemented Controller and repositories in Catalog.API

--- src/Services/Catalog/Catalog.API/Startup.cs
+++ src/Services/Catalog/Catalog.API/Startup.cs
@@ -1,15 +1,12 @@
+using Catalog.API.Data;
+using Catalog.API.Repositories;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Catalog.API
 {
@@ -25,12 +22,31 @@ namespace Catalog.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllers();
+            #region Configuration Dependencies
+
+            //services.Configure<CatalogDatabaseSettings>(Configuration.GetSection(nameof(CatalogDatabaseSettings)));
+
+            //services.AddSingleton<ICatalogDatabaseSettings>(sp =>
+            //    sp.GetRequiredService<IOptions<CatalogDatabaseSettings>>().Value);
+
+            #endregion
+
+            #region Project Dependencies
+
+            services.AddTransient<ICatalogContext, CatalogContext>();
+            services.AddTransient<IProductRepository, ProductRepository>();
+
+            #endregion
+
+            #region Swagger Dependencies
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Catalog.API", Version = "v1" });
             });
+
+            #endregion
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
