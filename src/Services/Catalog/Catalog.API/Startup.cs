1079ae7e4a2afe4b4d05c7e09c6b7c9c1ce6b759
Viviane dos Santos <vivianecruz01@hotmail.com>
2021-05-11 17:48:38 -0300

    Create docker compose and added dockerfile to Catalog.API

--- src/Services/Catalog/Catalog.API/Startup.cs
+++ src/Services/Catalog/Catalog.API/Startup.cs
@@ -5,7 +5,6 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Options;
 using Microsoft.OpenApi.Models;
 
 namespace Catalog.API
